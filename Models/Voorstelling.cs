9e59a41e4ae76f597c282e90dfb13edf565c6ee7
BramS21113653 <101334896+BramS21113653@users.noreply.github.com>
2022-12-23 13:06:33 +0100

    fixes

--- Models/Voorstelling.cs
+++ Models/Voorstelling.cs
@@ -6,7 +6,7 @@ public class Voorstelling {
     public string titel { get; set; }
     public string omschrijving { get; set; }
     public int prijs { get; set; }
-    public void addGroep(string groep) {
+    public void addGroep(Groep groep) {
         groepen.Add(groep);
     }
 
